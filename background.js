af433c5473e657188d048d90f64b9282acc600d4
Akshay Sapra <michaelsapra@Hotmail.com>
2024-03-26 22:23:04 -0400

    Change to version 3

--- background.js
+++ background.js
@@ -1,22 +1,21 @@
-chrome.runtime.onMessage.addListener(function(message,sender) {
+chrome.runtime.onMessage.addListener(function(message, sender) {
   if (message.type === 'middle-click') {
-    console.log(sender)
-        chrome.windows.getAll(function(windows) {
-        	    console.log(windows)
+    console.log(sender);
+    chrome.windows.getAll({populate: true}, function(windows) {
       if (windows.length > 1) {
-      	      var otherWindow = windows.filter(function(w){
-        return w.id != sender.tab.windowId;
-      })[0];
-
-        // There is more than one window open
-        // Open a new tab in the other window
-        chrome.tabs.create({url: message.link, windowId: otherWindow.id});
+        var otherWindow = windows.find(w => w.id !== sender.tab.windowId);
+        if (otherWindow) {
+          // There is more than one window open
+          // Open a new tab in the other window
+          chrome.tabs.create({ url: message.link, windowId: otherWindow.id });
+        } else {
+          console.error('Other window not found.');
+        }
       } else {
         // There is only one window open
         // Open a new tab in the current window
-        chrome.tabs.create({url: message.link});
+        chrome.tabs.create({ url: message.link });
       }
     });
-
   }
 });
--- content.js
+++ content.js
@@ -1,23 +1,16 @@
 document.addEventListener('auxclick', function(event) {
-
   if (event.button === 1) {
-
     // Get the link element that was clicked
     let linkElement = event.target;
     while (linkElement && linkElement.tagName !== 'A') {
       linkElement = linkElement.parentNode;
     }
 
-        // If a link element was found, open it in an existing window
+    // If a link element was found, open it in an existing window
     if (linkElement) {
-
       event.preventDefault();
-    // Send a message to the background script
-    chrome.runtime.sendMessage({ type: 'middle-click', link:linkElement.href});
-
+      // Send a message to the background script
+      chrome.runtime.sendMessage({ type: 'middle-click', link: linkElement.href });
     }
-
-
   }
-
 });
